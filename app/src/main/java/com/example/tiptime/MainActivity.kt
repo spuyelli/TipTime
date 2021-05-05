212db9a3bb65505b6e989834650d3d9935ae4e4e
spuyelli <spuyelli@gmail.com>
2021-05-04 21:47:40 -0300

    Se crea evento para el botón. Se desarrolla función para cálculo

--- app/src/main/java/com/example/tiptime/MainActivity.kt
+++ app/src/main/java/com/example/tiptime/MainActivity.kt
@@ -11,5 +11,11 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        binding.calculateButton.setOnClickListener { calculateTip() }
+    }
+
+    fun calculateTip(){
+        
     }
 }
