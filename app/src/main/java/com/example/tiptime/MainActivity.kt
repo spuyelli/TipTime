4975c05a1507da6375027e58f42c590a4db528e4
spuyelli <spuyelli@gmail.com>
2021-05-04 23:28:46 -0300

    .

--- app/src/main/java/com/example/tiptime/MainActivity.kt
+++ app/src/main/java/com/example/tiptime/MainActivity.kt
@@ -16,12 +16,12 @@ class MainActivity : AppCompatActivity() {
         binding.calculateButton.setOnClickListener { calculateTip() }
     }
 
-    fun calculateTip(){
+    fun calculateTip() {
         val stringInTextField = binding.costOfService.text.toString()
         val cost = stringInTextField.toDouble()
 
         val selectedId = binding.tipOptions.checkedRadioButtonId
-        val tipPercentage = when (selectedId){
+        val tipPercentage = when (selectedId) {
             R.id.option_twenty_percent -> 0.20
             R.id.option_eighteen_percent -> 0.18
             else -> 0.15
@@ -30,7 +30,7 @@ class MainActivity : AppCompatActivity() {
         var tip = tipPercentage * cost
 
         val roundUp = binding.roundUpSwitch.isChecked
-        if(roundUp){
+        if (roundUp) {
             tip = kotlin.math.ceil(tip)
         }
 
@@ -38,4 +38,5 @@ class MainActivity : AppCompatActivity() {
 
         binding.tipResult.text = getString(R.string.tip_amount, formattedTip)
     }
+
 }
